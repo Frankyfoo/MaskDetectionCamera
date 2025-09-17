f617635cf3cd4f754d4e28f71d1d1482bcea8ace
Frankyfoo <frankyfoo92@gmail.com>
2025-09-17 13:27:22 +0800

    Initial Commit

---
+++ MaskDetectionCamera/MaskDetectionCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaskDetectionCameraApp.swift
+//  MaskDetectionCamera
+//
+//  Created by Franky on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MaskDetectionCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
